755f45521886e4ea8c799c7b7de956923d3f6645
nnewtrp <newnack2013@gmail.com>
2026-02-20 16:33:14 +0700

    Initial Commit

---
+++ src/main/java/com/codeburger/codeburgerapi/CodeBurgerApiApplication.java
@@ -0,0 +1,13 @@
+package com.codeburger.codeburgerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBurgerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeBurgerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeburger/codeburgerapi/CodeBurgerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeburger.codeburgerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeBurgerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
