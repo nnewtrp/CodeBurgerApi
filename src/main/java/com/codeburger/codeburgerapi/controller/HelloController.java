43d95fc7cae1da3cf71c8c44044641aafedf4104
nnewtrp <newnack2013@gmail.com>
2026-02-21 23:56:27 +0700

    Create controller package

--- src/main/java/com/codeburger/codeburgerapi/controller/HelloController.java
+++ src/main/java/com/codeburger/codeburgerapi/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.codeburger.codeburgerapi;
+package com.codeburger.codeburgerapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
