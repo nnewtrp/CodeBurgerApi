9c8c3f4480aa9d477dad5d01bd0b058460517278
Teerapat Satitporn <newnack2013@gmail.com>
2026-02-27 13:41:20 +0700

    Create MasMenuController

---
+++ src/main/java/com/codeburger/codeburgerapi/controller/MasMenuController.java
@@ -0,0 +1,34 @@
+package com.codeburger.codeburgerapi.controller;
+
+import com.codeburger.codeburgerapi.dto.response.DataResponse;
+import com.codeburger.codeburgerapi.dto.response.ErrorResponse;
+import com.codeburger.codeburgerapi.entity.MasMenu;
+import com.codeburger.codeburgerapi.service.MasMenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.*;
+
+@RestController
+@RequestMapping("master/menu")
+public class MasMenuController {
+    @Autowired
+    private MasMenuService masMenuService;
+
+    @GetMapping("/{name}")
+    public ResponseEntity<?> getMenuInfo(@PathVariable String name) {
+        Optional<MasMenu> data = masMenuService.retrieveInfo(name);
+        if (data.isEmpty()) {
+            return ResponseEntity.badRequest().body(new ErrorResponse("Data NotFound"));
+        }
+        return ResponseEntity.ok(new DataResponse<>(data));
+    }
+}
