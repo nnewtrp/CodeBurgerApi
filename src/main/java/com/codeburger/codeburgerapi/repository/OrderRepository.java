2575897a34308b5ef327326e3e4c20411687e236
nnewtrp <newnack2013@gmail.com>
2026-03-07 23:55:02 +0700

    Create OrderRepository

---
+++ src/main/java/com/codeburger/codeburgerapi/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.codeburger.codeburgerapi.repository;
+
+import com.codeburger.codeburgerapi.entity.Order;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface OrderRepository extends MongoRepository<Order, String> {
+    Optional<Order> findByCustomerName(String customerName);
+}
